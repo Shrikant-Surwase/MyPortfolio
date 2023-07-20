687809becfdceceecca18e3c2f4aca0207bde1ee
Shrikant-Surwase <surwaseshrikant1729@gmail.com>
2023-07-20 10:23:56 +0530

    8Th Commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Notfound from "./pages/404";
 import "./app.css";
 
 
+
 function App() {
 	
 
@@ -26,7 +27,9 @@ function App() {
 				<Route path="/articles" element={<Articles />} />
 				<Route path="/article/:slug" element={<ReadArticle />} />
 				<Route path="/contact" element={<Contact />} />
+			
 				<Route path="*" element={<Notfound />} />
+				
 			
 			</Routes>
 		</div>
